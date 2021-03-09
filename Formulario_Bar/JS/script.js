d3df9a4d06001a24c32957701f6c8320922b47ff
ANDRES <78391158+PedroArenas0408@users.noreply.github.com>
2021-03-08 23:40:01 -0500

    Add files via upload

---
+++ Formulario_Bar/JS/script.js
@@ -0,0 +1,20 @@
+function ege(){
+let edad=""
+let i = 0
+
+document.getElementById("ege").innerHTML.value
+for(let i = 0; i<18; i++){
+  swal({
+    title:"Prohibido",
+    text:"El ingreso a menores de edad",
+    icon:"Error"
+  })
+  for( let j = 0; j >=18 && j<=80;j--){
+    swal({
+      title:"Acceso garantizado"
+    })
+  }
+}
+}
+
+document.write()
---
+++ Formulario_Bar/README.md
@@ -0,0 +1,2 @@
+# Formulario_Bar
+# Formulario_Bar
